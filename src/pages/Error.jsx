35bad0ba80f8ee7b1edccd303421661c499564f9
Rodolpho Reis <rodolphoreis@live.com>
2024-02-18 09:20:15 +0000

    feat: Add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,36 @@
+import { useRouteError, Link, useNavigate } from "react-router-dom";
+
+// css
+import "./error.css";
+
+// library
+import { ArrowUturnLeftIcon, HomeIcon } from "@heroicons/react/24/solid";
+
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <h2>Error</h2>
+    <div className="error-container">
+      <div className="error-message">
+        <h2>Uh oh! Nós temos um problema</h2>
+        <p>{error.message || error.statusText}</p>
+      </div>
+      <div className="error-btn">
+        <button
+          className="btn-goBack"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <ArrowUturnLeftIcon />
+          <span>Voltar</span>
+        </button>
+        <Link to="/" className="error-btn-dark">
+          <HomeIcon />
+          Home
+        </Link>
+      </div>
     </div>
   );
 };
