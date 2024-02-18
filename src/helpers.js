a65976d73ea5797d3aa0e741789f69de8997e566
Rodolpho Reis <rodolphoreis@live.com>
2024-02-18 19:48:27 +0000

    feat: Added waait function for asynchronous delay

--- src/helpers.js
+++ src/helpers.js
@@ -1,3 +1,9 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const waait = () =>
+  new Promise((res) => setTimeout(res, Math.random() * 2000));
+
+// colors
 const generateRandomColor = () => {
   const existingBudgetLength = fetchData("budgets")?.length ?? 0;
   return `${existingBudgetLength * 34} 65% 50%`;
@@ -13,14 +19,19 @@ export const createBudget = ({ name, amount }) => {
   console.log("createBudget foi chamada!");
   console.log("Criando novo orçamento:", name, amount);
   const newItem = {
-    id: crypto.randomUUID(),
+    id: Date.now().toString(),
     name: name,
     createAt: Date.now(),
     amount: +amount,
     color: generateRandomColor(),
   };
   const existingBudgets = fetchData("budgets") ?? [];
-  return localStorage.setItem(
+  const updatedBudgets = [...existingBudgets, newItem];
+
+  console.log("Orçamentos existentes:", existingBudgets);
+  console.log("Novos orçamentos:", updatedBudgets);
+
+  localStorage.setItem(
     "budgets",
     JSON.stringify([...existingBudgets, newItem])
   );
@@ -28,5 +39,11 @@ export const createBudget = ({ name, amount }) => {
 
 // delete item
 export const deleteItem = ({ key }) => {
+  clearLocalStorage();
   return localStorage.removeItem(key);
+};
+
+// clear localStorage
+const clearLocalStorage = () => {
+  localStorage.clear();
 };
