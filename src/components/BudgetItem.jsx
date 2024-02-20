1298babbe69b13f4100436a60df8eb71b7fa99b3
Rodolpho Reis <rodolphoreis@live.com>
2024-02-20 05:30:15 +0000

    feat: Create new components

---
+++ src/components/BudgetItem.jsx
@@ -0,0 +1,68 @@
+// rrd imports
+import { Form, Link } from "react-router-dom";
+
+// library imports
+import { BanknotesIcon, TrashIcon } from "@heroicons/react/24/outline";
+
+// helper functions
+import {
+  calculateSpentByBudget,
+  formatCurrency,
+  formatPercentage,
+} from "../helpers";
+
+const BudgetItem = ({ budget, showDelete = false }) => {
+  const { id, name, amount, color } = budget;
+  const spent = calculateSpentByBudget(id);
+
+  return (
+    <div
+      className="budget"
+      style={{
+        "--accent": color,
+      }}
+    >
+      <div className="progress-text">
+        <h3>{name}</h3>
+        <p>{formatCurrency(amount)} Orçamento</p>
+      </div>
+      <progress max={amount} value={spent}>
+        {formatPercentage(spent / amount)}
+      </progress>
+      <div className="progress-text">
+        <small>{formatCurrency(spent)} Gasto</small>
+        <small>{formatCurrency(amount - spent)} Restante</small>
+      </div>
+      {showDelete ? (
+        <div className="flex-sm">
+          <Form
+            method="post"
+            action="delete"
+            onSubmit={(event) => {
+              if (
+                !confirm(
+                  "Tem certeza de que deseja excluir permanentemente este orçamento?"
+                )
+              ) {
+                event.preventDefault();
+              }
+            }}
+          >
+            <button type="submit" className="btn">
+              <span>Deletar Orçamento</span>
+              <TrashIcon width={20} />
+            </button>
+          </Form>
+        </div>
+      ) : (
+        <div className="flex-sm">
+          <Link to={`/budget/${id}`} className="btn">
+            <span>Ver Detalhes</span>
+            <BanknotesIcon width={20} />
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+export default BudgetItem;
